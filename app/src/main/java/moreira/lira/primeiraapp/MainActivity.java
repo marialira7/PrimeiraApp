d1acecf3a22d62a485a7f33ef0c8c096b6f4ee24
marialira7 <marialiramoreira7@gmail.com>
2024-03-22 09:10:07 -0300

    Comentando o código s2

--- app/src/main/java/moreira/lira/primeiraapp/MainActivity.java
+++ app/src/main/java/moreira/lira/primeiraapp/MainActivity.java
@@ -13,15 +13,32 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //le o arquivo de layout e constri a interface
         setContentView(R.layout.activity_main);
+
+        // obtem o botao enviar
         Button btnEnviar=findViewById(R.id.btnEnviar);
+
+        //defini a acao do click no botao
         btnEnviar.setOnClickListener(new View.OnClickListener() {
+
+            // o metodo e chamado quando ha um click no botao
             @Override
             public void onClick(View v) {
+                //obter o campo de texto
                 EditText etDigiteAqui=findViewById(R.id.etDigiteAqui);
+
+                //obtem o texto que foi digitado na caixa de texto pelo usuario
                 String textoDigitado = etDigiteAqui.getText().toString();
+
+                //cria uma intençao para navegar para proxima tela
                 Intent i = new Intent(MainActivity.this,NextActivity.class);
+
+                //coloca o texto dentro da inteçao
                 i.putExtra("texto", textoDigitado);
+
+                //executa a intençao e vai para proxima tela
                 startActivity(i);
 
             }
--- app/src/main/java/moreira/lira/primeiraapp/NextActivity.java
+++ app/src/main/java/moreira/lira/primeiraapp/NextActivity.java
@@ -11,15 +11,20 @@ public class NextActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         //exibir elementos do usuário na tela
         setContentView(R.layout.activity_next);
 
+        //obtem a intençao q criou a tela
         Intent i = getIntent();
 
-        //criando variável
+        //criando variável derivado da tela anterior
         String texto = i.getStringExtra("texto");
 
+        //obtem o textview
         TextView tvTexto = findViewById(R.id.etTexto);
+
+        //seta o texto dentro do textview
         tvTexto.setText(texto);
     }
 }
